f298ad2abf961ff716cbbec7d705e818787b9a5f
RASOLOFONIAINA Mickael <rasolofoniaina_mickael@outlook.fr>
2023-06-05 20:21:55 +0300

    MaxSubArraySum solution

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -24,3 +24,5 @@ console.log(countUniqueValues([1, 1, 1, 1, 1, 2, 3, 3, 10])); // 2
 console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));  // count: 11,  unique values: 7
 console.log(countUniqueValues([])); // 0
 console.log(countUniqueValues([-2, -1, -1, 0, 1])); // 4
+
+// TO-DO: Write another function which uses 2 pointers
---
+++ maxSubArraySum.js
@@ -0,0 +1,54 @@
+// Problem: Compute the max sum of n adjacent elements
+// Input: array of nunbers
+// Output: max sum values
+
+// Naive
+// Complexity in time O(^2)
+// Complexity in space O(1)
+
+function maxSubArraySum(array, n) {
+  if(n > array.length) return null;
+
+  var max = -Infinity;
+
+  for(var i = 0; i <= array.length - n; i++) { 
+    var sum = 0;
+    for(var j = i; j < i + n; j++) {
+      sum += array[j]; 
+    } 
+    max = Math.max(max, sum);
+  }
+
+  return max;
+}
+
+// Complexity in time O(n)
+// Complexity in space O(1)
+function maxSubArraySum(array, n) {
+  if(n > array.length) return null;
+
+  var sum = 0;
+  var temp = 0;
+  var max = 0;
+
+  for(var i = 0; i < n; i++) {
+    sum += array[i];
+    max = sum;
+  }
+
+  for(var i = 1; i <= array.length - n; i++) {
+    temp = sum - array[i - 1] + array[i + n - 1]; 
+    max = Math.max(max, temp);
+    sum = temp;
+  }
+
+  return max;
+}
+
+console.log(maxSubArraySum([1,2,5,2,8,1,5], 2)) // 10
+console.log(maxSubArraySum([1,2,5,2,8,1,5], 4)) // 17
+console.log(maxSubArraySum([4,2,1,6], 1)) // 6
+console.log(maxSubArraySum([4,2,1,6,2], 4)) // 13
+console.log(maxSubArraySum([], 4)) // null
+
+
